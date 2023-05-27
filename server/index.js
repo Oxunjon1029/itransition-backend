9dfbbbcad675a513ee351058295f22472f161ae4
Oxunjon1029 <abdusalomov2019oxunjon@gmail.com>
2023-05-27 19:13:25 +0500

    last commit

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use('/api', userRouter)
 app.use(express.static(path.join(__dirname + '/public')))
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URL);
+    await connectDB(process.env?.MONGO_URL);
     app.listen(PORT, () => {
       console.log(`Server is running on PORT ${PORT}`);
     });

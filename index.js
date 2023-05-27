15840feae8dd3b3c285df7e2d862799b4a75a0be
Oxunjon1029 <abdusalomov2019oxunjon@gmail.com>
2023-05-27 19:38:18 +0500

    make it better

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const authRouter = require("../src/routes/auth");
-const userRouter = require('../src/routes/users');
+const authRouter = require("./src/routes/auth");
+const userRouter = require('./src/routes/users');
 const app = express()
 const PORT = process.env.PORT || 5000
-const connectDB = require("../src/db/connectDb");
+const connectDB = require("./src/db/connectDb");
 
 app.use(cors())
 app.use(express.json());
